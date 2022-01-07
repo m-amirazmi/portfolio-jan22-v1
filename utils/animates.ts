8b161475ecaf892e657781153fa7b5d56a5ca5d6
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-01-08 00:15:46 +0800

    remove delay

--- utils/animates.ts
+++ utils/animates.ts
@@ -109,7 +109,7 @@ export const animateReveal = {
 	initial: "hidden",
 	whileInView: "visible",
 	viewport: { once: true },
-	transition: { duration: 0.3, delay: 0.2 },
+	transition: { duration: 0.3 },
 	variants: {
 		visible: { opacity: 1, scale: 1 },
 		hidden: { opacity: 0, scale: 0.9 },
